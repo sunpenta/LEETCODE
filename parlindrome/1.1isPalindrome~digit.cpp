14a186b186a971fb30ade8f00d4aa97b72db3ff8
Auly <sunpentacle@icloud.com>
2023-04-10 13:55:06 +0000

    Apr 10, 2023, 9:55 PM

--- parlindrome/1.1isPalindrome~digit.cpp
+++ parlindrome/1.1isPalindrome~digit.cpp
@@ -12,10 +12,9 @@ int main()
 
 bool isPalindrome(int x)
 {
-    // 特殊情况：负数,10的倍数不是回文数
+    // 特殊情况：不是回文数：负数或， 10的倍数（除0）
     if (x < 0 || (x%10==0 && x!=0)) return false;
-    if (x < 10) 
-        return true;
+    if (x < 10)  return true; // 个位数
 
     int reverted = 0;
     int xx = x;
