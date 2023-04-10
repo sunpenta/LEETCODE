c8b61f779aead766668f7aa2cf44404469dd48d0
Auly <sunpentacle@icloud.com>
2023-04-10 16:04:43 +0000

    Apr 11, 2023, 12:04 AM

--- Prefix Sum/2countSubarrays.cpp
+++ Prefix Sum/2countSubarrays.cpp
@@ -28,7 +28,7 @@ int countSubarrays(vector<int>& nums, int k)
     int sum=0, ans=0;
     for (int i=0;i<nums.size();i++) 
     {
-        sum+=sign(nums[i]-k); 
+        sum+=sign(nums[i]-k); // nums[i], >k->1, <k->-1, =k->0
         if (i<kIndex)
             counts[sum]++; // left=i+1 
         else
