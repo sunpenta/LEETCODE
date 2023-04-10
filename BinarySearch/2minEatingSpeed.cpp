ca86243037fecad7c8ed09a0230827c6ec708424
Auly <sunpentacle@icloud.com>
2023-04-10 16:49:19 +0000

    Apr 11, 2023, 12:49 AM

--- BinarySearch/2minEatingSpeed.cpp
+++ BinarySearch/2minEatingSpeed.cpp
@@ -1,2 +1,24 @@
 // 吃香蕉-binary search  l-875
 // piles.length∈[1,1e4], piles[i]∈[1,1e9]
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int minEatingSpeed(vector<int>& piles, int h);
+
+int main()
+{
+    vector<int> piles={30,11,23,4,20}; int h=5; // input
+    cout<<minEatingSpeed(piles,h)<<endl; // output:30
+    system("pause");
+}
+// speed is k, eat all piles
+bool check(vector<int>& piles, int h, int k)
+{
+    
+}
+int minEatingSpeed(vector<int>& piles, int h)
+{
+    int left=1, right=*max_element(piles.begin(),piles.end());
+
+}
