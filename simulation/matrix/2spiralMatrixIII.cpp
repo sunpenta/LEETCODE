d19707264e8b8e3222af493865f93efa6e23921f
Auly <sunpentacle@icloud.com>
2023-04-10 14:17:50 +0000

    Apr 10, 2023, 10:17 PM

--- simulation/matrix/2spiralMatrixIII.cpp
+++ simulation/matrix/2spiralMatrixIII.cpp
@@ -7,5 +7,25 @@ vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart);
 
 int main()
 {
-    
+    int rows=5, cols=6, rStart=1, cStart=4; // input
+    vector<vector<int>> path=spiralMatrixIII(rows,cols,rStart,cStart); // output
+    for (auto row:path)
+        cout<<"("<<row[0]<<", "<<row[1]<<")"<<endl;
+    system("pause");
+}
+
+vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart)
+{
+    vector<vector<int>> path;
+    int dx=0, dy=1, tmp;
+    for (int n=0; path.size()<rows*cols-1; n++)
+    {
+        for (int i=0; i<n/2+1; i++)
+        {
+            rStart+=dx; cStart+=dy;
+            if (rStart>=0 && rStart<rows && cStart>=0 && cStart<cols)
+                path.push_back({rStart,cStart});
+        }
+        tmp=dx, dx=dy, dy=-tmp;
+    }
 }
