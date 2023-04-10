a0103fa7c31fe33287f50f1cb5dd8edecc9fdfa4
Auly <sunpentacle@icloud.com>
2023-04-10 23:48:55 +0000

    Apr 11, 2023, 7:48 AM

--- Prefix Sum/2.1pivotIndex.cpp
+++ Prefix Sum/2.1pivotIndex.cpp
@@ -1,1 +1,27 @@
 // 中位数的位置-prefix sum  l-724
+// index: sum[0,index)=sum(index,n-1], 若找不到index, 返回-1
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+int pivotIndex(vector<int>& nums);
+
+int main()
+{
+    vector<int> nums={1,7,3,6,5,6}; // input
+    cout<<pivotIndex(nums)<<endl; // output:3 1+7+3 = 5+6 =11
+    system("pause");
+}
+
+int pivotIndex(vector<int>& nums)
+{
+    int n=nums.size();
+    int leftsum=0, rightsum=accumulate(nums.begin(),nums.end(),0);
+    for (int i=0; i<n; i++)
+    {
+        rightsum-=nums[i];
+        if (leftsum==rightsum) return i;
+        leftsum+=nums[i];
+    }
+    return -1;
+}
