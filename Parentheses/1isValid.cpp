f49cfd75e7bb5ca4ab25f9bbfb975f8a23e09721
Auly <sunpentacle@icloud.com>
2023-04-10 10:06:12 +0800

    some message

--- Parentheses/1isValid.cpp
+++ Parentheses/1isValid.cpp
@@ -6,7 +6,7 @@ bool isValid(string s);
 int main()
 {
     string s="{()[()]}"; // 输入
-    cout<<boolalpha<<isValid(s)<<endl; // 输出:1
+    cout<<boolalpha<<isValid(s)<<endl; // 输出:true
     system("pause");
 }
 
@@ -15,25 +15,25 @@ bool isValid(string s)
     int n=s.size();
     // 字符串为大小为奇数时，输出false
     if (n%2==1) return false;
-    // 键为右括号，值为左括号
-    unordered_map<char,char> pairs=
+
+    unordered_map<char,char> pairs= // <right，left>
     {
         {')','('},
         {']','['},
         {'}','{'},
     };
 
-    stack<char> stk; // 建立栈,存char
-    for (char ch: s) // 遍历字符串s中的字符
-    {    // ch是右括号
-        if (pairs.count(ch)) // count()内是键
-        {  // 当前左括号数必大于右括号数，因此栈必不为空
-           if (stk.empty() || stk.top()!=pairs[ch])
-                return false; // 栈为空或栈顶元素不是对应左括号，输出false
-            stk.pop(); // 一对括号匹配，栈顶元素出栈  
+    stack<char> stk; 
+    for (char ch: s)
+    {   
+        if (pairs.count(ch)) // ch是右括号
+        {  
+           if (stk.empty() || stk.top()!=pairs[ch]) // 当前左括号数必大于右括号数，因此栈必不为空
+                return false; // 栈为空, 或栈顶元素!=对应左括号，输出false
+            stk.pop(); // 一对括号匹配，左括号出栈  
         }
-        else // 字符ch是左括号，入栈
+        else // ch是左括号，入栈
             stk.push(ch);     
     }
-    return stk.empty(); // 若栈为空，是有效括号
+    return stk.empty(); // 若栈为空，left==right,是有效括号
 }
