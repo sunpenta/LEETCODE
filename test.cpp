8822a85595badd5de5c949e257de4cc2b14396e0
Auly <sunpentacle@icloud.com>
2023-04-11 02:44:23 +0000

    Apr 11, 2023, 10:44 AM

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int minbike(vector<int>& heights, int m);
+int main()
+{
+    int m, n;
+    cin>>m>>n;
+    vector<int>heights;
+    for (int i=0; i<n; i++)
+    {
+        cin>>heights[i];
+    }
+    cout<<minbike(heights,m)<<endl;
+}
+
+int minbike(vector<int>& heights, int m)
+{
+    int n=heights.size();
+    int left=0, right=n-1;
+    int ans=0; // min num of bikes
+    while (left<right)
+    {
+        if (heights[left]+heights[right]>m)
+        {
+            right--;ans++;
+        }
+        else
+        {
+            left++; right--; ans++;
+        }
+    }
+    return ans;
+}
