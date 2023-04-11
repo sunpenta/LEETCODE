f4c6d4af365739a974c0056b0a267ec8fd38c719
Auly <sunpentacle@icloud.com>
2023-04-11 00:11:23 +0000

    Apr 11, 2023, 8:11 AM

--- hash Table/2checkInclusion.cpp
+++ hash Table/2checkInclusion.cpp
@@ -1,1 +1,31 @@
-// Permutation in string - hash tabl
+// Permutation in string - hash table  l-567
+// s2 include s1's permutation
+#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
+bool checkInclusion(string s1, string s2);
+
+int main()
+{
+    string s1="ab", s2=; // input
+    cout<<boolalpha<<checkInclusion(s1,s2)<<endl; // output
+    system("pause");
+}
+
+bool checkInclusion(string s1, string s2)
+{
+    int m=s1.length(), n=s2.length();
+    array<int, 26> mp1, mp2;
+    for (char ch:s1) mp1[ch-'a']++;
+
+    for (int i=0; i<m; i++) mp2[s2[i]-'a']++;
+    if (mp1==mp2) return true;
+    for (int i=m; i<n; i++)
+    {
+        --mp2[s2[i-m]-'a'];
+        ++mp2[s2[i]-'a'];
+        if (mp1==mp2) return true;
+    }
+    return false;
+}
