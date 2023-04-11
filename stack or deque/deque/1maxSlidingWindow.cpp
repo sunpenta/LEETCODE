3e9e9fa0912198573fd2a952ee4415218488fa09
Auly <sunpentacle@icloud.com>
2023-04-11 01:22:22 +0000

    Apr 11, 2023, 9:22 AM

--- stack or deque/deque/1maxSlidingWindow.cpp
+++ stack or deque/deque/1maxSlidingWindow.cpp
@@ -20,7 +20,7 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k)
     vector<int> res;
     for (int i=0; i<n; i++)
     {
-        if (!dq.empty() && dq.front()==i-k) dq.pop_front();
+        if (!dq.empty() && dq.front()==i-k) dq.pop_front(); // [i-k+1,i]
         while (!dq.empty() && nums[i]>nums[dq.back()]) dq.pop_back();
         dq.emplace_back(i);
         if (i>=k-1) res.emplace_back(dq.front());
