d07118b71d0b531f98fbb9a6616080c4187237a2
Auly <sunpentacle@icloud.com>
2023-04-11 14:24:27 +0000

    Apr 11, 2023, 10:24 PM

---
+++ string/1.3reverseIII.cpp
@@ -0,0 +1,26 @@
+// reverse words in string - 原地修改 l-557
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string reverseWords(string s);
+
+int main()
+{
+    string s="Let's take LeetCode contest"; // input
+    cout<<reverseWords(s)<<endl; // output
+    system("pause");
+}
+
+string reverseWords(string s)
+{
+    int n=s.length();
+    for (int i=0; i<n; i++)
+    {
+        int j=i; // next space or end
+        for (;j<n && j<n; j++) {};
+        reverse(s.begin()+i, s.begin()+j);
+        i=j-1;
+    }
+    return s;
+}
