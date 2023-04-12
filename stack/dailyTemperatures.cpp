c57dad2c794fb916fc26fe9d54939d8d9547318d
Auly <sunpentacle@icloud.com>
2023-04-12 09:08:26 +0000

    Apr 12, 2023, 5:08 PM

---
+++ stack/dailyTemperatures.cpp
@@ -0,0 +1,31 @@
+// 每日温度 - stack   l-739
+// 等？天有更高温度; 时间复杂度:O(n) 空间复杂度:O(1)
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+vector<int> dailyTemperatures(vector<int>& temperatures);
+
+int main()
+{
+    vector<int> temperatures={73,74,75,71,69,72,76,73}; // input
+    vector<int> res=dailyTemperatures(temperatures); // output: {1,1,4,2,1,1,0,0}
+    for (int i:res) cout<<i<<" ";
+}
+
+vector<int> dailyTemperatures(vector<int>& temperatures)
+{
+    int n=temperatures.size();
+    stack<int> sk; // higher temp's index
+    vector<int> res(n);
+    for (int i=0; i<n; i++)
+    {
+        while (!sk.empty() && temperatures[i]>temperatures[sk.top()])
+        {
+            int tmp=sk.top(); sk.pop();
+            res[tmp]=i-tmp;
+        }
+        sk.push(i);
+    }
+    return res;
+}
