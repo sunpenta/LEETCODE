f2b0b22f502f4c0207c1b1a9333024927851afb2
Auly <sunpentacle@icloud.com>
2023-04-12 04:05:07 +0000

    Apr 12, 2023, 12:05 PM

--- string/1.2reverseII.cpp
+++ string/1.2reverseII.cpp
@@ -1,6 +1,7 @@
-// reverse k-length string per 2k-length  l-541
+// reverse k-length string per 2k- Group  l-541
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 string reverseII(string s, int k);
 
@@ -12,4 +13,12 @@ int main()
 string reverseII(string s, int k)
 {
     int n=s.length();
+    for (int i=0; i<n; i+=2*k)
+    {
+        if (n-i<k)
+            reverse(s.begin()+i,s.end());
+        else
+            reverse(s.begin()+i, s.begin()+k);
+    }
+    return s;
 }
