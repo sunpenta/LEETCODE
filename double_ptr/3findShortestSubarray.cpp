c13e335313df5818ab9b3b98d503628678a08ae2
Auly <sunpentacle@icloud.com>
2023-04-17 16:07:28 +0000

    Apr 18, 2023, 12:07 AM

--- double_ptr/3findShortestSubarray.cpp
+++ double_ptr/3findShortestSubarray.cpp
@@ -1,13 +1,14 @@
-// 删锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷剩锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷-双指锟斤拷  l-1574  -DP/vector/1 | prefixSum
-// nums.length鈭圼1,1e5]
-#include <bits/stdc++.h>
+//  移除最短子数组使数组有序 -˫ָ��  l-1574  -DP/vector/1 | prefixSum
+// nums.length∈[1,1e5]
+#include <iostream>
+#include <vector>
 using namespace std;
 int findShortestSubarray(vector<int>& nums);
 
 int main()
 {
     vector<int>nums={1,2,2,2,3,1,7,5,1,2,2,2,2,5,6}; // input
-    cout<<findShortestSubarray(nums)<<endl; // output:5 剩锟斤拷锟斤拷锟斤拷:{1,2,2,2,2,2,2,2,5,6}
+    cout<<findShortestSubarray(nums)<<endl; // output:5 ʣ������:{1,2,2,2,2,2,2,2,5,6}
     return 0;
 }
 
@@ -18,14 +19,14 @@ int findShortestSubarray(vector<int>& nums)
 
     while (right>=0 && nums[right-1]<=nums[right])
         --right;
-    if (right==0) return 0; // 原锟斤拷锟斤拷锟斤拷锟斤拷
-    int ans=right; // 删锟斤拷[0,right-1]
+    if (right==0) return 0; // ԭ��������
+    int ans=right; // ɾ��[0,right-1]
 
     for (left=0;left==0 || nums[left-1]<=nums[left];left++)
     {
         while (right<n && nums[left]>nums[right]) 
             ++right;
-        ans=min(ans,right-left-1); // 删锟斤拷(left,right)
+        ans=min(ans,right-left-1); // ɾ��(left,right)
     }
     return ans;
 }
