e89b263c6a4636532c0965ab3858a84ebc4e5e60
Auly <sunpentacle@icloud.com>
2023-04-17 15:28:04 +0000

    Apr 17, 2023, 11:28 PM

--- array/1removeDuplicates.cpp
+++ array/1removeDuplicates.cpp
@@ -1,33 +1,30 @@
 // 删除有序数组中的重复项 - 新数组 l-26  -Linked List/delete/deleteDuplicates
+// -> 无重复数组个数
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> deleteDuplicates(vector<int>& nums);
+int deleteDuplicates(vector<int>& nums);
 
 int main()
 { 
-    vector<int> nums{1,1,2}; // 输入
-
-    vector<int>ans;
-    int n=nums.size();
-    for (int i=0;i<n;i++)
-    {
-        if (i<n-1 && nums[i]==nums[i+1]) 
-            continue;
-        ans.push_back(nums[i]);
-    }
-    int k=ans.size();
-
-    for (int i=0;i<k;i++)
-        nums[i]=ans[i];
-
-    // 输出
-    cout<<k<<endl;
-    for (auto num:nums) 
-        cout<< num <<" ";
-    system("pause");  
+    vector<int> nums={0,0,1,1,1,2,2,3,3,4}; // 输入
+    cout<<deleteDuplicates(nums)<<endl; // output: 5
+    for (int num:nums) // 改变原数组
+        cout<< num <<" "; // 0 1 2 3 4
+    return 0;  
 }
 
-vector<int> deleteDuplicates(vector<int>& nums)
+int deleteDuplicates(vector<int>& nums)
 {
+    int n=nums.size();
+    vector<int> res;
+    for (int i=0;i<n;i++)
+    {
+        if (i>0 && nums[i]==nums[i-1]) 
+            continue;
+        res.emplace_back(nums[i]);
+    }
+    int k=res.size();
 
+    for (int i=0;i<k;i++)
+        nums[i]=res[i];
 }
