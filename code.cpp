d11f17783c0b2e0c7d591594eb97d1dc40a37986
Auly <sunpentacle@icloud.com>
2023-04-18 14:42:22 +0000

    Apr 18, 2023, 10:42 PM

--- code.cpp
+++ code.cpp
@@ -5,22 +5,24 @@ using namespace std;
 string mergeAlternately(string word1, string word2) {
      int n1=word1.length(), n2=word2.length();
      string s="";
+     int i=0, j=0;
      for (int i=0, j=0; i<n1 && j<n2; ++i, ++j)
      {
-          if (i==n1-1) 
+
+          if ((word1[i]-'a')<=(word2[j]-'a'))
+          {
+               s+=word1[i]; 
                s+=word2[j];
-          else if (j==n2-1) 
-               s+=word1[i];
+          }
           else
           {
-               if ((word1[i]-'a')<=(word2[j]-'a'))
-               {
-                    s+=word1[i]; s+=word2[j];
-               }
-               else
-                    s+=word2[j];s+=word1[i];
+               s+=word2[j];
+               s+=word1[i];
           }
+          
      } 
+     while (i<n1) s+=word1[i++];
+     while (j<n2) s+=word2[j++];
      return s;     
 }
 int main()
