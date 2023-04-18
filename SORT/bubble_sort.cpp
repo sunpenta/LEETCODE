7b2c582848ad411d1e25020c49e068b49b6f2505
Auly <sunpentacle@icloud.com>
2023-04-18 10:13:13 +0000

    Apr 18, 2023, 6:13 PM

--- SORT/bubble_sort.cpp
+++ SORT/bubble_sort.cpp
@@ -7,21 +7,21 @@ int main()
 {
     int arr[]={3,2,5,7,4}; int n=5;
     bubble_sort(arr, n);
-    //for (int num:arr)
-        //cout<<num<<" ";
+    for (int num:arr)
+        cout<<num<<" ";
     return 0;
 }
 
 void bubble_sort(int arr[], int n)
 {
-    // int tmp;
-    for (int i=n-1; i>0; ++i)
+    int tmp;
+    for (int i=n-1; i>0; --i)
     {
         for (int j=0; j<i; ++j)
         {
             if (arr[j]>arr[j+1])
             {
-                int tmp=arr[j];
+                tmp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=tmp;
             }
