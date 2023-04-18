0809032b87ae861b99c2f0f69ff377e22ae11d4d
Auly <sunpentacle@icloud.com>
2023-04-18 10:00:17 +0000

    Apr 18, 2023, 6:00 PM

--- SORT/bubble_sort.cpp
+++ SORT/bubble_sort.cpp
@@ -14,14 +14,14 @@ int main()
 
 void bubble_sort(int arr[], int n)
 {
-    int tmp;
+    // int tmp;
     for (int i=n-1; i>0; ++i)
     {
         for (int j=0; j<i; ++j)
         {
             if (arr[j+1]<arr[j])
             {
-                tmp=arr[j];
+                int tmp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=tmp;
             }
