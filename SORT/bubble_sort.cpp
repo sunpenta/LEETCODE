f2e7f9864655883b974cb245282d21950ab8e47b
Auly <sunpentacle@icloud.com>
2023-04-18 11:17:30 +0000

    Apr 18, 2023, 7:17 PM

--- SORT/bubble_sort.cpp
+++ SORT/bubble_sort.cpp
@@ -1,5 +1,5 @@
 // 冒泡排序
-// 时间复杂度：最好：O(n), 最坏：O(n²), 平均：O(n²); 空间复杂度：O(n)
+// 时间复杂度：最好：O(n), 最坏：O(n²), 平均：O(n²); 空间复杂度：O(1)
 #include <iostream>
 using namespace std;
 void bubble_sort(int arr[], int n);
--- SORT/bubble_sort~optimized.cpp
+++ SORT/bubble_sort~optimized.cpp
@@ -1,2 +1,35 @@
 // 优化的冒泡排序
 // 时间复杂度：最好：O(n), 最坏：O(n²), 平均：O(n²); 空间复杂度：O(1)
+#include <iostream>
+using namespace std;
+void bubble_sort(int arr[], int n);
+
+int main()
+{
+    int arr[]={3,2,5,7,4}; int n=5;
+    bubble_sort(arr, n);
+    for (int num:arr)
+        cout<<num<<" ";
+    cout<<endl;
+    return 0;
+}
+
+void bubble_sort(int arr[], int n)
+{
+    int tmp; bool flag;
+    for (int i=n-1; i>0; --i)
+    {
+        flag=false;
+        for (int j=0; j<i; ++j)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                tmp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=tmp;
+                flag=true;
+            }
+            if (!flag) break;
+        }
+    }
+}
