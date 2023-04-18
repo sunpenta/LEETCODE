4f1d9e1f20a4ec0901aa7471491954e670668470
Auly <sunpentacle@icloud.com>
2023-04-18 14:48:23 +0000

    Apr 18, 2023, 10:48 PM

--- code.cpp
+++ code.cpp
@@ -6,10 +6,10 @@ string mergeAlternately(string word1, string word2) {
      int n1=word1.length(), n2=word2.length();
      string s="";
      int i=0, j=0;
-     for (int i=0, j=0; i<n1 && j<n2; ++i, ++j)
+     for (i=0, j=0; i<n1 && j<n2; ++i, ++j)
      {
 
-          if ((word1[i]-'a')<=(word2[j]-'a'))
+          if (word1[i]<=word2[j])
           {
                s+=word1[i]; 
                s+=word2[j];
@@ -29,6 +29,7 @@ int main()
 {    
      string word1="abc"; string word2="bcd";
      string s=mergeAlternately(word1, word2);
-     cout<<s<<endl;
+     string ss=""+'a'+'b';
+     cout<<ss<<endl;
      return 0;
 }
