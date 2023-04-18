540dec928fb2af5596b80f421fa3264cb241d703
Auly <sunpentacle@icloud.com>
2023-04-18 14:10:26 +0000

    Apr 18, 2023, 10:10 PM

--- SORT/merge_sort.cpp
+++ SORT/merge_sort.cpp
@@ -3,20 +3,32 @@
 #include <iostream>
 using namespace std;
 void merge_sort(int arr[], int l, int r);
-void print(int arr, int n);
+void print(int arr[], int n);
 int main()
 {
-
+    int arr[]={9,8,6,7,2,1}; int n=sizeof(arr)/sizeof(arr[0]);
+    print(arr,n);
+    merge_sort(arr,0,n-1);
+    print(arr,n);
 }
 
-void merge(int arr[], int l, int mid, int r)
+void merge(int arr[], int l, int mid, int r) // [l,mid] [mid+1,r]
 {
     int copy[r-l+1];
     int i=l, j=mid+1, k=l;
-    while (i<mid && j<r)
+    while (i<=mid && j<=r)
     {
-        
+        if (arr[i]<arr[j])
+            copy[k++]=arr[i++];
+        else
+            copy[k++]=arr[j++];
     }
+    while (i<=mid)
+        copy[k++]=arr[i++];
+    while (j<=r)
+        copy[k++]=arr[j++];
+    for (int i=0; i<r-l+1; ++i) // copy->arr
+        arr[i]=copy[i];
 }
 void merge_sort(int arr[], int l, int r)
 {
@@ -26,7 +38,9 @@ void merge_sort(int arr[], int l, int r)
    merge_sort(arr,mid+1, r); 
    merge(arr,l,mid,r);
 }
-void print(int arr, int n)
+void print(int arr[], int n)
 {
-
+    for (int i=0; i<n; ++i)
+        cout<<arr[i]<<" ";
+    cout<<endl;
 }
