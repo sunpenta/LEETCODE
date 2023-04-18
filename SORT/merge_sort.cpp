2010eaad4947f6f80b73a628c86e63887b416538
Auly <sunpentacle@icloud.com>
2023-04-18 14:53:01 +0000

    Apr 18, 2023, 10:53 PM

--- SORT/merge_sort.cpp
+++ SORT/merge_sort.cpp
@@ -1,5 +1,5 @@
 // 归并排序
-// 时间复杂度: T(n)=T(n/2)+O(n) O(logn); 空间复杂度:O(logn)
+// 时间复杂度: T(n)=2*T(n/2)+O(n) O(logn); 空间复杂度:O(logn)
 #include <iostream>
 using namespace std;
 void merge_sort(int arr[], int l, int r);
