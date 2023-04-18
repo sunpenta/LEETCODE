cf0c2f207484045ca9f506c7ec1b783507670cc7
Auly <sunpentacle@icloud.com>
2023-04-18 14:04:20 +0000

    Apr 18, 2023, 10:04 PM

--- SORT/merge_sort.cpp
+++ SORT/merge_sort.cpp
@@ -8,3 +8,25 @@ int main()
 {
 
 }
+
+void merge(int arr[], int l, int mid, int r)
+{
+    int copy[r-l+1];
+    int i=l, j=mid+1, k=l;
+    while (i<mid && j<r)
+    {
+        
+    }
+}
+void merge_sort(int arr[], int l, int r)
+{
+   if (l>=r) return;
+   int mid=l+(r-l)/2;
+   merge_sort(arr, l, mid);
+   merge_sort(arr,mid+1, r); 
+   merge(arr,l,mid,r);
+}
+void print(int arr, int n)
+{
+
+}
