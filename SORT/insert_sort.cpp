3c3f2bb1220c0ea39d9bf5e7fb83b3ad5b05505a
Auly <sunpentacle@icloud.com>
2023-04-18 12:30:33 +0000

    Apr 18, 2023, 8:30 PM

---
+++ SORT/insert_sort.cpp
@@ -0,0 +1,25 @@
+// 插入排序
+// 时间复杂度：最好：O(n) 最坏：O(n²) 平均：O(n²) ；空间复杂度：O(1)
+#include <iostream>
+using namespace std;
+void insert_sort(int arr[], int n);
+
+int main()
+{
+    int arr[]={3,2,5,7,4}; int n=5;
+    insert_sort(arr, n);
+    for (int num:arr)
+        cout<<num<<" ";
+    cout<<endl;
+    return 0;
+}
+void insert_sort(int arr[], int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        int tmp=arr[i], j;
+        for (j=i; j<n && arr[j-1]>tmp; ++j)
+            arr[j]=arr[j-1];
+        arr[j]=tmp;
+    }
+}
