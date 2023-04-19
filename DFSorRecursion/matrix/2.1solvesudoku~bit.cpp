c17de47de06a2e1bd85a9517f067b55ad0a23f70
Auly <sunpentacle@icloud.com>
2023-04-19 12:13:53 +0000

    Apr 19, 2023, 8:13 PM

--- DFSorRecursion/matrix/2.1solvesudoku~bit.cpp
+++ DFSorRecursion/matrix/2.1solvesudoku~bit.cpp
@@ -5,6 +5,7 @@
 等价于  mask &= (mask - 1) // 消掉最低位的1   */
 #include <bits/stdc++.h>
 using namespace std;
+void solveSudoku(vector<vector<char>>& board);
 int main()
 {
     vector<vector<char>>board=    
@@ -29,65 +30,62 @@ int main()
     }
     system("pause");
 }
-class Solution{
-private://类内使用
-    int row[9];
-    int column[9];
-    int block[3][3];
-    bool valid;
-    vector<pair<int, int>> spaces;
-public:
-    //  把1从低到高数移到第digit+1位
-    void flip(int i, int j, int digit) 
-    {
-        row[i] ^= (1 << digit); // ^=,异或赋值，不同取1，相同取0
-        column[j] ^= (1 << digit); // 1左移digit位
-        block[i / 3][j / 3] ^= (1 << digit);
-    }
 
-    void dfs(vector<vector<char>>& board, int pos) 
-    {
-        if (pos == spaces.size()) 
-        {
-            valid = true;
-            return;
-        }
-        auto [i, j] = spaces[pos];
-        int mask = ~(row[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;//9位二进制记录没出现数字  & 0x1ff？多余
-        // 1.当且仅当mask!=0时才有没出现过的digit
-        // 2.mask &= (mask - 1)代表移除mask中最低位的1,表示已经被搜索 如:10100 & 10011 = 10000
-        for (; mask && !valid; mask &= (mask - 1)) 
-        {
-            int digitMask = mask & (-mask); // 等价mask & (~mask + 1)，获取某个二进制数的 LowBit，lowbit(x)是x的二进制表达式中最低位的1所对应的值
-            int digit = __builtin_ctz(digitMask); // 返回digitMask的二进制末尾0的个数
-            flip(i, j, digit);
-            board[i][j] = digit + '0' + 1;
-            dfs(board, pos + 1);
-            flip(i, j, digit); //// 撤回标记+恢复现场
-        }
-    }
+int row[9];
+int column[9];
+int block[3][3];
+bool valid;
+vector<pair<int, int>> spaces;
+//  把1从低到高数移到第digit+1位
+void flip(int i, int j, int digit) 
+{
+    row[i] ^= (1 << digit); // ^=,异或赋值，不同取1，相同取0
+    column[j] ^= (1 << digit); // 1左移digit位
+    block[i / 3][j / 3] ^= (1 << digit);
+}
 
-    void solveSudoku(vector<vector<char>>& board) 
+void dfs(vector<vector<char>>& board, int pos) 
+{
+    if (pos == spaces.size()) 
     {
-        memset(row, 0, sizeof(row)); // 以字节为单位赋值0
-        memset(column, 0, sizeof(column)); // 0 0000 0000
-        memset(block, 0, sizeof(block)); //  0 0001 0000
-        valid = false;
-        for (int i = 0; i < 9; ++i) 
+        valid = true;
+        return;
+    }
+    auto [i, j] = spaces[pos];
+    int mask = ~(row[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;//9位二进制记录没出现数字  & 0x1ff？多余
+    // 1.当且仅当mask!=0时才有没出现过的digit
+    // 2.mask &= (mask - 1)代表移除mask中最低位的1,表示已经被搜索 如:10100 & 10011 = 10000
+    for (; mask && !valid; mask &= (mask - 1)) 
+    {
+        int digitMask = mask & (-mask); // 等价mask & (~mask + 1)，获取某个二进制数的 LowBit，lowbit(x)是x的二进制表达式中最低位的1所对应的值
+        int digit = __builtin_ctz(digitMask); // 返回digitMask的二进制末尾0的个数
+        flip(i, j, digit);
+        board[i][j] = digit + '0' + 1;
+        dfs(board, pos + 1);
+        flip(i, j, digit); //// 撤回标记+恢复现场
+    }
+}
+
+void solveSudoku(vector<vector<char>>& board) 
+{
+    memset(row, 0, sizeof(row)); // 以字节为单位赋值0
+    memset(column, 0, sizeof(column)); // 0 0000 0000
+    memset(block, 0, sizeof(block)); //  0 0001 0000
+    valid = false;
+    for (int i = 0; i < 9; ++i) 
+    {
+        for (int j = 0; j < 9; ++j) 
         {
-            for (int j = 0; j < 9; ++j) 
+            if (board[i][j] == '.') 
             {
-                if (board[i][j] == '.') 
-                {
-                    spaces.emplace_back(i, j); // 记录空格坐标
-                }
-                else 
-                {
-                    int digit = board[i][j] - '0' - 1; // 1-9 char转换成0-8 int
-                    flip(i, j, digit);
-                }
+                spaces.emplace_back(i, j); // 记录空格坐标
+            }
+            else 
+            {
+                int digit = board[i][j] - '0' - 1; // 1-9 char转换成0-8 int
+                flip(i, j, digit);
             }
         }
-        dfs(board, 0);
     }
-};
+    dfs(board, 0);
+}
