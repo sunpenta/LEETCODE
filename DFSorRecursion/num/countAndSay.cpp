e50decad9b8a6b09cb6f3f1f5bf1588ebb2e887b
Auly <sunpentacle@icloud.com>
2023-04-19 13:45:01 +0000

    Apr 19, 2023, 9:45 PM

--- DFSorRecursion/num/countAndSay.cpp
+++ DFSorRecursion/num/countAndSay.cpp
@@ -6,7 +6,7 @@ string countAndSay(int n);
 int main()
 {  
     int n=5; // 输入
-    cout<<countAndSay(n)<<endl; // 输出: 
+    cout<<countAndSay(n)<<endl; // 输出: 111221
     return 0;
 }
 
@@ -15,16 +15,16 @@ string countAndSay(int n)
     if (n==1) return "1";
         
     string str=""; // 初始化放在if后面，否则溢出
-    string str_last=countAndSay(n-1); // 递归
+    string pre_str=countAndSay(n-1); // 递归
     int len=1;
-    for (int i=0;i<str_last.size();i++)
+    for (int i=0; i<pre_str.size(); i++)
     {   
-        if (str_last[i]==str_last[i+1])
+        if (pre_str[i]==pre_str[i+1])
             len++;
         else
         {
-            str+=to_string(len)+str_last[i]; // +=省内存
-            len=1;               
+            str+=to_string(len)+pre_str[i]; // +=省内存
+            len=1;            
         }
     }
     return str;
