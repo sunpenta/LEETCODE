0cb451f615d73f75f9e4b6374ae11436092dcd48
Auly <sunpentacle@icloud.com>
2023-04-19 12:28:01 +0000

    Apr 19, 2023, 8:28 PM

--- DFSorRecursion/matrix/4.2shortestpath~obstacle.cpp
+++ DFSorRecursion/matrix/4.2shortestpath~obstacle.cpp
@@ -1,5 +1,5 @@
 // 迷宫最短路径-dfs
-// 有障碍；方向：下，右；输出：位置[i,j]
+// 有障碍；方向：下，右；-> 位置[i,j]
 #include <bits/stdc++.h>
 using namespace std;
 vector<vector<int>>shortestPath(vector<vector<int>>& grid);
@@ -9,18 +9,19 @@ int main()
     vector<vector<int>> grid = {
         {0, 0, 1},
         {0, 1, 0},
-        {0, 0, 0}}; // input
+        {0, 0, 0}
+    }; // input
     vector<vector<int>> ans = shortestPath(grid); // output: {(0,0),(1,0),(2,0),(2,1),(2,2)}
     for (auto loc : ans)
     {
         cout << "(" << loc[0] << "," << loc[1] << ")" << endl;
     }
-    system("pause");
+    return 0;
 }
-bool dfs(vector<vector<int>>&ans,vector<vector<int>>& grid, int row,int col)
+bool dfs(vector<vector<int>>& ans, vector<vector<int>>& grid, int row, int col)
 {
     int m=grid.size(), n=grid[0].size();
-    if (row>=m || col>=n || grid[row][col]==1) return false;
+    if (row>=m || col>=n || grid[row][col]==1) return false; // 越界，有障碍
     if (row==m-1 && col==n-1)
     {
         ans.push_back({m-1,n-1});
@@ -28,15 +29,14 @@ bool dfs(vector<vector<int>>&ans,vector<vector<int>>& grid, int row,int col)
     }
     ans.push_back({row,col});
     if (dfs(ans,grid,row+1,col) || dfs(ans,grid,row,col+1))
-     return true;
-
-    ans.pop_back();
+        return true;
+    ans.pop_back(); // 回溯
     return false;
 }
 
 vector<vector<int>>shortestPath(vector<vector<int>>& grid)
 {
-    vector<vector<int>>ans;
+    vector<vector<int>> ans;
     dfs(ans,grid,0,0);
     return ans;
 }
