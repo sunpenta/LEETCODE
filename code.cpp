b5ddc83a120e1d5463ad5ec484333c019490b57a
Auly <sunpentacle@icloud.com>
2023-04-20 12:22:27 +0000

    Apr 20, 2023, 8:22 PM

--- code.cpp
+++ code.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-string mergeAlternately(string word1, string word2) {
-     int n1=word1.length(), n2=word2.length();
-     string s="";
-     int i=0, j=0;
-     for (i=0, j=0; i<n1 && j<n2; ++i, ++j)
-     {
-
-          if (word1[i]<=word2[j])
-          {
-               s+=word1[i]; 
-               s+=word2[j];
-          }
-          else
-          {
-               s+=word2[j];
-               s+=word1[i];
-          }
-          
-     } 
-     while (i<n1) s+=word1[i++];
-     while (j<n2) s+=word2[j++];
-     return s;     
-}
+bool reach(vector<vector<int>>& maze, int rowStart, int colStart, int endStart, int colEnd);
 int main()
 {    
-     string word1="abc"; string word2="bcd";
-     string s=mergeAlternately(word1, word2);
-     string ss=""+'a'+'b';
-     cout<<ss<<endl;
+     cout<<endl;
      return 0;
+}
+
+bool dfs(vector<vector<int>>& maze, int rowEnd, int colEnd, int i, int j)
+{
+     int m=maze.size(), n=maze[0].size();
+     if (i<0 || i>=m || j<0 || j>=n) return false; // 越界
+     if (i==rowEnd && j==colEnd) return true;
+     if (maze[i][j]==-1) return false; // 已访问
+     maze[i][j]=-1; // 标记
+     if (maze[i][j]==1) return false;
+     int dx[4]={1,-1,0,0};
+     int dy[4]={0,0,1,-1};
+     for (int d=0; d<4; ++d)
+     {
+          int nx=i+dx[d], ny=j+dy[d];
+          if (dfs(maze,rowEnd,colEnd,nx,ny))
+               return true;
+     }
+     return false;
+}
+bool reach(vector<vector<int>>& maze, int rowStart, int colStart, int rowEnd, int colEnd)
+{
+     return dfs(maze,rowEnd,colEnd,rowStart,colStart);
 }
