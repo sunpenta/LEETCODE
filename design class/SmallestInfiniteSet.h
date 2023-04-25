41f3286b776b5a764aa367e78d6f5fe512859250
Auly <sunpentacle@icloud.com>
2023-04-25 09:24:10 +0000

    Apr 25, 2023, 5:24 PM

--- design class/SmallestInfiniteSet.h
+++ design class/SmallestInfiniteSet.h
@@ -1,18 +1,32 @@
 /**
  * Your SmallestInfiniteSet object will be instantiated and called as such:
  * SmallestInfiniteSet* obj = new SmallestInfiniteSet(); 初始化为所有正整数num⋿[1,1000]
- * int param_1 = obj->popSmallest();
- * obj->addBack(num);
+ * int param_1 = obj->popSmallest(); 移除并返回集合内的最小值
+ * obj->addBack(num); 若集合不存在num，加入集合
  */
 #include <set>
 class SmallestInfiniteSet
 {
-    set<int> nums; // set内元素自动排序
+    set<int> nums; // set元素自动排序
 public:
     SmallestInfiniteSet()
     {
         for (int i=1; i<1000; ++i)
             nums.insert(i);
     }
-    ~SmallestInfiniteSet();
+    int popSmallest()
+    {
+        int smallest;
+        if (!nums.empty())
+        {
+            smallest=*nums.begin();
+            nums.erase(nums.begin());
+        }
+        return smallest;
+    }
+    addBack(int num)
+    {
+        if (!nums.count(num))
+            nums.insert(num);
+    }
 };
