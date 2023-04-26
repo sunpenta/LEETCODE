09c25115ba81b79f12bf885524de6a1a5366ffda
Auly <sunpentacle@icloud.com>
2023-04-26 15:12:14 +0000

    Apr 26, 2023, 11:12 PM

--- parlindrome/1.2isPalindrome~str.cpp
+++ parlindrome/1.2isPalindrome~str.cpp
@@ -1,4 +1,4 @@
-// 验证回文串-存入新字符串  l-125
+// 回文串 - 存入新字符串  l-125
 // 把所有大写字母转成小写字母，并移除所有非字母和数字字符，剩余字符串是回文串; s.length∈[1,2e5]
 #include <bits/stdc++.h>
 using namespace std;
@@ -6,20 +6,20 @@ bool isPalindrome(string s);
 
 int main()
 {
-    string s="0P"; // 输入
+    string s="0P:"; // 输入
     cout<<boolalpha<<isPalindrome(s)<<endl; // 输出:false
     system("pause");
 }
 
 bool isPalindrome(string s)
 {
-    int n=s.length();
-    string news; // 存入新字符串
+    string t;
     for (char &ch:s)
     {
         if (isalnum(ch)) // 字母或数字
-            news+=tolower(ch);
+            t+=tolower(ch);
     }
-    string rs(news.rbegin(),news.rend()); // reverse
-    return news==rs; // "0p"!="p0"
+    s=t;
+    reverse(t.begin(),t.end());
+    return s==t; // "0p"!="p0"
 }
