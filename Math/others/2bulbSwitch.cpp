01f3f3b68cb66de454c10d55173bf3e13505642c
Auly <sunpentacle@icloud.com>
2023-04-27 11:40:12 +0000

    Apr 27, 2023, 7:40 PM

---
+++ Math/others/2bulbSwitch.cpp
@@ -0,0 +1,19 @@
+// 灯泡切换 - math  l-319
+// 1 round: turn all on, 2 round: turn 2-th off; 3 round: toggle 3-th, ..., i round: toggle i-th;n⋿[0,1e9], 直接模拟TLE
+#include <iostream>
+#include <cmath>
+using namespace std;
+int bulbSwitch(int n);
+
+int main()
+{
+    int n=5; // input;
+    cout<<bulbSwitch(n)<<endl; // output
+    return 0;
+}
+int bulbSwitch(int n)
+{
+    int i=1;
+    for (; pow(i,2)<=n; i++) {}
+    return i-1;
+}
