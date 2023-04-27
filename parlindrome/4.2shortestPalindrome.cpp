fef4b9178b6d642dc6a30e0de6e5eef6f4bf2554
Auly <sunpentacle@icloud.com>
2023-04-27 14:44:39 +0000

    Apr 27, 2023, 10:44 PM

--- parlindrome/4.2shortestPalindrome.cpp
+++ parlindrome/4.2shortestPalindrome.cpp
@@ -1,1 +1,17 @@
-// 最短回文串
+// 最短回文串 - recursion  l-214
+// 在string前加字母，使它成为最短回文串；length⋿[0,5e4]
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string shortestPalindrome(string s);
+
+int main()
+{
+
+}
+
+string shortestPalindrome(string s)
+{
+    int n=s.length();
+    
+}
