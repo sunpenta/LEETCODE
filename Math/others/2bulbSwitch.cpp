5664a54c067f5c66f1df829dd739a3b739c49297
Auly <sunpentacle@icloud.com>
2023-04-27 11:47:51 +0000

    Apr 27, 2023, 7:47 PM

--- Math/others/2bulbSwitch.cpp
+++ Math/others/2bulbSwitch.cpp
@@ -11,6 +11,8 @@ int main()
     cout<<bulbSwitch(n)<<endl; // output:2
     return 0;
 }
+// 第i轮后，前i个灯泡不变；经奇数次切换，最终是ON，例[ON][ON,OFF,ON], 经偶数次切换，最终是OFF，例[ON,OFF][ON,OFF,ON,OFF]
+// 结果是有奇数次切换的个数，即有奇数个除数的个数，即有完全平方根的个数（例，1[1,1]奇 3[1,3]偶 4[1,4][2,2]奇）
 int bulbSwitch(int n)
 {
     int i=1;
