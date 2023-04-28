f507f9751b5fac9e3966c133adc1d867d1efb6d0
Auly <sunpentacle@icloud.com>
2023-04-28 15:32:08 +0000

    Apr 28, 2023, 11:32 PM

---
+++ DP/array/minTotalDistance.cpp
@@ -0,0 +1,33 @@
+// 机器人最小遍历距离 - dp  l-2463
+// robot.length, factory.length⋿[1,100], robot, position⋿[-1e9,1e9]
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+long long minTotalDiatance(vector<int>& robot, vector<vector<int>>& factory);
+
+int main()
+{
+    vector<int> robot={0,4,6}; vector<vector<int>> factory={{2,2},{6,2}}; // input
+    cout<<minTotalDiatance(robot,factory)<<endl; // output:4 |0-2|+|4-2|+|6-6|=4
+    return 0;
+}
+
+long long minTotalDiatance(vector<int>& robot, vector<vector<int>>& factory)
+{
+    int n=robot.size(), m=factory.size();
+    sort(robot.begin(),robot.end());
+    sort(factory.begin(),factory.end());
+    vector<long long> dp(n+1,1e15); // dp[i]:repair i robot min总距离; note:<long long>
+    for (int i=0; i<m; ++i)
+    {
+        for (int k=0; k<factory[i][1]; ++k)
+        {
+            for (int j=n-1; j>=0; --j)
+            {
+                dp[j+1]=min(dp[i+1],dp[i]+abs(factory[i][0]-robot[j]));
+            }
+        }
+    }
+    return dp[n];
+}
