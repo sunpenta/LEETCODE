340c0013bd53b919d733bcbeabb6911d618373ec
Auly <sunpentacle@icloud.com>
2023-05-04 13:24:34 +0000

    May 4, 2023, 9:24 PM

--- Greed/smallestSubsequence.cpp
+++ Greed/smallestSubsequence.cpp
@@ -7,7 +7,6 @@ string smallestSubsequence(string s, int k, char letter, int repetition);
 
 int main()
 {
-    cout<<(unsigned int) -1<<endl;
     string s="leet"; int k=3, repetition=1; char letter='e'; // input
     //cout<<smallestSubsequence(s,k,letter,repetition)<<endl; // "eet"
     return 0;
@@ -28,7 +27,8 @@ string smallestSubsequence(string s, int k, char letter, int repetition)
         char c=s[i];
         
         bool flag2=(k-sk.size()<repetition);
-        while (!sk.empty() && ((c<sk.top() && sk.size()+n-i>k && (sk.top()!=letter || sk.top()==letter && cnt>repetition)) 
+        while (!sk.empty() && ((c<sk.top() && sk.size()+n-i>k && 
+        (sk.top()!=letter || sk.top()==letter && cnt>repetition)) 
         || k-(int)sk.size()<repetition)) // sk.size() - unsigned int
         {
             char cur=sk.top();
