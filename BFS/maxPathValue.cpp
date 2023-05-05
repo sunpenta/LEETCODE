be89052e2bee2e4edadaff0e7086981b9e0327bd
Auly <sunpentacle@icloud.com>
2023-05-05 06:59:34 +0000

    May 5, 2023, 2:59 PM

---
+++ BFS/maxPathValue.cpp
@@ -0,0 +1,17 @@
+// 有向图的最多颜色值 - bfs  l-1857
+// 时间复杂度: 空间复杂度:
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace;
+int maxPathValue(string color, vector<vector<int>>& edges);
+
+int main()
+{
+
+}
+
+int maxPathValue(string color, vector<vector<int>>& edges)
+{
+    vector<vector<int>> graph
+}
