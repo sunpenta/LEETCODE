2e345b3137366075057c960645b4b3cffb8f60b1
Auly <sunpentacle@icloud.com>
2023-05-05 06:52:49 +0000

    May 5, 2023, 2:52 PM

--- DP/num/1countVowelStrings.cpp
+++ DP/num/1countVowelStrings.cpp
@@ -1,4 +1,4 @@
-// 字典序元音字符串的组合数-dp  l-1641
+// 字典序元音字符串的组合数 - dp  l-1641
 // 字符串长度为n,仅由元音(a,e,i,o,u)组成，按字典序排列; 元素可重复使用; n∈[1,50]
 #include <iostream>
 #include <vector>
@@ -10,15 +10,15 @@ int main()
 {
     int n=3; // 输入
     cout<<countVowelStrings(n)<<endl; // 输出
-    system("pause");
+    return 0;
 }
 
 int countVowelStrings(int n)
 {
-    vector<int>dp(5,1); // dp[i]:以i为结尾的字符串数; 0:a 1:e 2:i 3:o 4:u
-    for (int i=1;i<n;i++) // i:字符串长度-1
+    vector<int> dp(5,1); // dp[i]:以i为结尾的字符串数; 0:a 1:e 2:i 3:o 4:u
+    for (int i=1; i<n; i++) // i:字符串长度-1
     {
-        for (int j=1;j<5;j++)
+        for (int j=1; j<5; j++)
         {
             dp[j]+=dp[j-1];
         }
--- DP/num/1countVowelStrings~math.cpp
+++ DP/num/1countVowelStrings~math.cpp
@@ -1,4 +1,4 @@
-// 字典序元音字符串的组合数-可重组合数  l-1641
+// 字典序元音字符串的组合数 - 可重组合数  l-1641
 // 字符串长度为n,仅由元音(a,e,i,o,u)组成，按字典序排列; 元素可重复使用; n∈[1,50]
 #include <iostream>
 using namespace std;
@@ -8,7 +8,7 @@ int main()
 {
     int n=2; // 输入
     cout<<countVowelStrings(n)<<endl; // 输出:15 {"aa","ae","ai","ao","au","ee","ei","eo","eu",...}
-    system("pause");
+    return 0;
 }
 // C(n+r-1,r)
 int countVowelStrings(int n)
