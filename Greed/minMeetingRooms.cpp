edd39e396f2ce2a5fc95b9e18cbf420f8466f128
Auly <sunpentacle@icloud.com>
2023-05-05 13:27:27 +0000

    May 5, 2023, 9:27 PM

--- Greed/minMeetingRooms.cpp
+++ Greed/minMeetingRooms.cpp
@@ -8,7 +8,7 @@ int minMeetingRooms(vector<vector<int>>& intervals);
 
 int main()
 {
-    vector<vector<int>> intervals={{1, 3}, {2, 6}, {8, 10}, {9, 15}, {12, 14}}; // input
+    vector<vector<int>> intervals={{1, 3}, {8, 10},{2, 6},  {9, 15}, {12, 14}}; // input
     cout<<minMeetingRooms(intervals)<<endl; // output:3
     return 0;
 }
@@ -17,7 +17,7 @@ int minMeetingRooms(vector<vector<int>>& intervals)
 {
     int n=intervals.size();
     int minRooms=1;
-    sort(intervals.begin(),intervals.end(),[](const vector<int>& a, const vector<int>& b){return a[1]<b[1];});
+    sort(intervals.begin(),intervals.end());
     for (int i=1; i<n; ++i)
     {
         if (intervals[i][0]>intervals[i-1][1])
