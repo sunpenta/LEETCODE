962dadbf96ebc28191b67e7338249ef0d4e04c9b
Auly <sunpentacle@icloud.com>
2023-05-05 07:30:10 +0000

    May 5, 2023, 3:30 PM

--- BFS/maxPathValue.cpp
+++ BFS/maxPathValue.cpp
@@ -1,19 +1,19 @@
 // 有向图的最多颜色值 - bfs  l-1857
-// 时间复杂度: 空间复杂度:
+// 时间复杂度: 空间复杂度:; 若有向图有环,返回-1
 #include <iostream>
 #include <vector>
 #include <queue>
 using namespace std;
-int maxPathValue(string colors, vector<vector<int>>& edges);
+int largestPathValue(string colors, vector<vector<int>>& edges);
 
 int main()
 {
     string colors="abaca"; vector<vector<int>>edges = {{0,1},{0,2},{2,3},{3,4}}; // input
-    cout<<maxPathValue(colors,edges)<<endl; // ouput:3
+    cout<<largestPathValue(colors,edges)<<endl; // ouput:3
     return 0;
 }
 
-int maxPathValue(string colors, vector<vector<int>>& edges)
+int largestPathValue(string colors, vector<vector<int>>& edges)
 {
     int n=colors.length();
     vector<vector<int>> graph(n); vector<int> indeg(n); // 初始化size
@@ -52,6 +52,7 @@ int maxPathValue(string colors, vector<vector<int>>& edges)
                 q.push(neighbor);
         }
     }
+    if (visit<n) return -1;
     int res=0;
     for (int i=0; i<n; ++i)
     {
