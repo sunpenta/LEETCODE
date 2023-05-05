f04a34b31a85abd85678ee967293a6bf7e7f940b
Auly <sunpentacle@icloud.com>
2023-05-05 12:13:03 +0000

    May 5, 2023, 8:13 PM

--- BinarySearch/searchInsert.cpp
+++ BinarySearch/searchInsert.cpp
@@ -3,15 +3,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int searchInsert(vector<int>& nums);
+int searchInsert(vector<int>& nums, int target);
 
 int main()
 {
-
+    vector<int> nums={2,3,5,7,8,9}; int target=6; // input
+    cout<<searchInsert(nums,target)<<endl; // output:3
+    return 0;
 }
 
-int searchInsert(vector<int>& nums)
+int searchInsert(vector<int>& nums, int target)
 {
     int left=0, right=nums.size()-1;
-    
+    while (left<=right)
+    {
+        int mid=(left+right)/2;
+        if (nums[mid]==target)
+            return mid;
+        else if (nums[mid]>target)
+            left=mid+1;
+        else
+            right=mid-1;
+    }
+    return left;
 }
