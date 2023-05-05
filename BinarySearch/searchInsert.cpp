0c2bfa7a869296c99079fd4238c964dd934e475a
Auly <sunpentacle@icloud.com>
2023-05-05 12:10:13 +0000

    May 5, 2023, 8:10 PM

--- BinarySearch/searchInsert.cpp
+++ BinarySearch/searchInsert.cpp
@@ -2,3 +2,16 @@
 // 不含重复元素，升序排列；length⋿[1,1e4]; 时间复杂度:O(logn)
 #include <iostream>
 #include <vector>
+using namespace std;
+int searchInsert(vector<int>& nums);
+
+int main()
+{
+
+}
+
+int searchInsert(vector<int>& nums)
+{
+    int left=0, right=nums.size()-1;
+    
+}
