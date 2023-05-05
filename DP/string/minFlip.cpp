4af7e00144ae1349708d6c8e701401dbaf5148d1
Auly <sunpentacle@icloud.com>
2023-05-05 03:47:33 +0000

    May 5, 2023, 11:47 AM

--- DP/string/minFlip.cpp
+++ DP/string/minFlip.cpp
@@ -1,2 +1,25 @@
 // flip string 使它单调递增 -dp  l-926
 #include <iostream>
+using namespace std;
+int minFlip(string s);
+
+int main()
+{
+    string s="0011101"; // input
+    cout<<minFlip(s)<<endl; // output:1  0011111
+    return 0;
+}
+
+int minFlip(string s)
+{
+    int count_one=0, count_flip=0;
+    for (char ch:s)
+    {
+        if (ch=='1')
+            ++count_one;
+        else
+            ++count_flip;
+        count_flip=min(count_flip,count_one);
+    }
+    return count_flip;
+}
