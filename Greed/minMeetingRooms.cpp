5441ca3e9455ee7b36fe943a3e5ae2da52441a5d
Auly <sunpentacle@icloud.com>
2023-05-05 12:40:29 +0000

    May 5, 2023, 8:40 PM

--- Greed/minMeetingRooms.cpp
+++ Greed/minMeetingRooms.cpp
@@ -1,2 +1,16 @@
 // 最少会议室 - greedy  l-253
-// 
+// intervals[i]:[si,ei]:si:start time, ei:end time; 时间复杂度:
+#include <iostream>
+#include <vector>
+using namespace std;
+int minMeetingRooms(vector<vector<int>>& intervals);
+
+int main()
+{
+
+}
+
+int minMeetingRooms(vector<vector<int>>& intervals)
+{
+
+}
