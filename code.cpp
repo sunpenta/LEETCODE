7d75d0cd725b13eb016f96bf14c8a080f3d2682f
Auly <sunpentacle@icloud.com>
2023-05-06 03:33:39 +0000

    May 6, 2023, 11:33 AM

--- code.cpp
+++ code.cpp
@@ -6,7 +6,7 @@ int mod=1e9+7;
         if (a==0 || a==1) return 1;
         return a*fact(a-1);
     }
-    int C(int a, int b)
+    long C(int a, int b)
     {
         int res=fact(a)/fact(b)/fact(a-b);
         return res;
