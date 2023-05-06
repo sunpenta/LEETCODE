461618b37478cf9013de330bb22a5b228f5ce5a0
Auly <sunpentacle@icloud.com>
2023-05-06 08:04:39 +0000

    May 6, 2023, 4:04 PM

--- code.cpp
+++ code.cpp
@@ -3,7 +3,7 @@ using namespace std;
     int mod=1e9+7;
     int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());  
-        int n=nums.size(), count=0;
+        int n=nums.size();long long count=0;
         if (nums[0]>target) return 0;
         int j=n-1;
         for (int i=0; i<n; ++i)
@@ -12,19 +12,19 @@ using namespace std;
                 ++count;
             else
                 break;
-            
             while (nums[i]+nums[j]>target)
             {
                 --j;
             }
-            count+=(pow(2,j-i)-1);
+            if (j>i)
+                count+=(long long)(pow(2,j-i)-1) % mod;
         }        
         return count%mod;
     }
     
 int main()
 {
-    vector<int> nums = {3,5,6,7}; int target = 9;
+    vector<int> nums = {6,10,12,3,29,21,12,25,17,19,16,1,2,24,9,17,25,22,12,22,26,24,24,11,3,7,24,5,15,30,23,5,20,10,19,20,9,27,11,4,23,4,4,12,22,27,16,11,26,10,23,26,16,21,24,21,17,13,21,9,16,17,27}; int target = 9;
     cout<<numSubseq(nums,target)<<endl;
     return 0;
 }
