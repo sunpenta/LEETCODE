54b9d0baa3a8fb1f15068b4f4e8add2fd1b24cd6
Auly <sunpentacle@icloud.com>
2023-05-08 07:48:17 +0000

    May 8, 2023, 3:48 PM

--- code.cpp
+++ code.cpp
@@ -1,51 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-    int smallestChair(vector<vector<int>>& _times, int targetFriend) {
-        priority_queue<int, vector<int>, greater<int> > chairs; // reserved chairs
-        int n=_times.size();
+    vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
+        int n=servers.size(), m=tasks.size();
+        vector<tuple<int,int,int>> times; // <time,type,index==start time>,1:start 0:end
+        for (int i=0; i<m; ++i)
+        {
+            times.push_back({i,1,i});
+            times.push_back({i+tasks[i],0,i});
+        }
+        sort(times.begin(),times.end());
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq; // servers
         for (int i=0; i<n; ++i)
         {
-            chairs.push(i);
+            pq.push({servers[i],i});
         }
-        vector<pair<int,int>> arrive; // <time,friend> 
-        vector<pair<int,int>> leave;  // <time,friend>
-        for (int i=0; i<n; ++i)
+        vector<int> ans(m);
+        for (auto& [time,type,idx]:times)
         {
-            arrive.push_back({_times[i][0],i});
-            leave.push_back({_times[i][1],i});
-        }
-        sort(arrive.begin(),arrive.end());
-        sort(leave.begin(),leave.end());
-        pair<int,int> target={_times[targetFriend][0],targetFriend};
-        int idx=find(arrive.begin(),arrive.end(),target)-arrive.begin();
-        int i=0, j=0;
-        vector<int> seat(n);
-        while (i<=idx)
-        {
-            if (arrive[i].first<leave[j].first)
-            {               
-                seat[arrive[i].second]=chairs.top();
-                chairs.pop();
-                ++i;
-            }
-            else if (arrive[i].first>leave[j].first)
+            if (type==1)
             {
-                chairs.push(seat[leave[j].second]); // j leave
-                ++j;
+                ans[idx]=pq.top().second;
+                pq.pop();
             }
             else
             {
-                seat[i]=seat[leave[j].second];
-                ++i, ++j;
+                pq.push({servers[ans[idx]],ans[idx]});
             }
         }
-        return seat[targetFriend];    
+        return ans;
     }
     
 int main()
 {
-    vector<vector<int>> times ={{1,5},{2,3},{3,9},{4,7},{5,8}}; int targetFriend =2;
-    cout<<smallestChair(times,targetFriend)<<endl;
+    vector<int> servers =
+    {10,63,95,16,85,57,83,95,6,29,71};
+    vector<int> tasks =
+    {70,31,83,15,32,67,98,65,56,48,38,90,5};
+    vector<int> ans=assignTasks(servers, tasks);
     // ***
     // long long a = (static_cast<int64_t>(pow(2, 54)) - 1) % mod; // int64_t is long long
     // ***
