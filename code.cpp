972883626444965264ec9a7aafa7c361803d9182
Auly <sunpentacle@icloud.com>
2023-05-08 08:04:47 +0000

    May 8, 2023, 4:04 PM

--- code.cpp
+++ code.cpp
@@ -2,22 +2,26 @@
 using namespace std;
     vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
         int n=servers.size(), m=tasks.size();
-        vector<tuple<int,int,int>> times; // <time,type,index==start time>,1:start 0:end
+        vector<pair<int,int>> start; // <time,index==start time>
+        vector<pair<int,int>> end;
         for (int i=0; i<m; ++i)
         {
-            times.push_back({i,1,i});
-            times.push_back({i+tasks[i],0,i});
+            start.push_back({i,i});
+            end.push_back({i+tasks[i],i});
         }
-        sort(times.begin(),times.end());
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq; // servers
         for (int i=0; i<n; ++i)
         {
             pq.push({servers[i],i});
         }
-        vector<int> ans(m);
-        for (auto& [time,type,idx]:times)
+        vector<int> ans(m); 
+        int i=0, j=0;
+        while (i<m && j<m)
         {
-            if (type==1)
+            int idx=start[i].second;
+            if (start[i].first<start[j].first)
             {
                 ans[idx]=pq.top().second;
                 pq.pop();
