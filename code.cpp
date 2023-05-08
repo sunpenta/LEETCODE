3618773d99a2f15bc7fc3d5f28ab5732036856c2
Auly <sunpentacle@icloud.com>
2023-05-08 12:53:55 +0000

    May 8, 2023, 8:53 PM

--- code.cpp
+++ code.cpp
@@ -1,49 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-    vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
-        int n=servers.size(), m=tasks.size();
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > times; // <end,server>
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > server; // servers
-        for (int i=0; i<n; ++i)
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        int maxnum=0;
+        for (int i=0; i<k; ++i)
         {
-            server.push({servers[i],i});
+            maxnum=max(maxnum,nums[i]);
         }
-        vector<int> ans(m);
-        for (int i=0; i<m; ++i)
+        vector<int> res(n-k+1);
+        for (int i=k; i<n; ++i)
         {
-            int start=i, end=start+tasks[i];
-            if (server.empty())
+            if (nums[i]>=maxnum)
             {
-                start=times.top().first, end=start+tasks[i];
-            } 
-            while (!times.empty() && times.top().first<=start)
-            {
-                int s=times.top().second;
-                server.push({servers[s],s});
-                times.pop();
+                res[i-k]=nums[i];
             }
-            
-            if (server.empty())
+            else if (nums[i]<maxnum && nums[i-k]<maxnum)
             {
-                start=times.top().first, end=start+tasks[i];
+                res[i-k]=maxnum;
             }
             else
             {
-                ans[i]=server.top().second;
-                server.pop();
-                times.push({end,ans[i]});
+                res[i-k]=*max_element(nums.begin()+i-k+1,nums.begin()+i);
             }
         }
-        return ans;
+        return res;
     }
     
 int main()
 {
-    vector<int> servers =
-    {10,63,95,16,85,57,83,95,6,29,71};
-    vector<int> tasks =
-    {70,31,83,15,32,67,98,65,56,48,38,90,5};
-    vector<int> ans=assignTasks(servers, tasks);
+    vector<int> nums =
+    {1,3,-1,-3,5,3,6,7};
+    int k=3;
+    vector<int> res=maxSlidingWindow(nums, k);
     // ***
     // long long a = (static_cast<int64_t>(pow(2, 54)) - 1) % mod; // int64_t is long long
     // ***
