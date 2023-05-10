8a838718879fadfbf41791bfbfa046e975791737
Auly <sunpentacle@icloud.com>
2023-05-10 10:54:37 +0000

    May 10, 2023, 6:54 PM

--- code.cpp
+++ code.cpp
@@ -15,30 +15,28 @@ using namespace std;
         }
         return i;
     }*/
-       int removeElement(vector<int>& nums, int val) {
-        int leftIndex = 0;
-        int rightIndex = nums.size() - 1;
-        while (leftIndex <= rightIndex) {
-            // 找左边等于val的元素
-            while (leftIndex <= rightIndex && nums[leftIndex] != val){
-                ++leftIndex;
-            }
-            // 找右边不等于val的元素
-            while (leftIndex <= rightIndex && nums[rightIndex] == val) {
-                -- rightIndex;
-            }
-            // 将右边不等于val的元素覆盖左边等于val的元素
-            if (leftIndex < rightIndex) {
-                nums[leftIndex++] = nums[rightIndex--];
-            }
+    bool backspaceCompare(string s, string t) {
+        string s1="", t1="";
+        for (int i=0,j=0; i<s.length() && j<t.length(); ++i, ++j)
+        {
+            if (s[i]!='#')
+                s1+=s[i];
+            else if (s[i]=='#' && s1.length()>0)
+                s1.pop_back();
+            if (t[j]!='#')
+                t1+=t[j];
+            else if (t[j]=='#' && t1.length()>0) // why do not not include add  t[j]=='#' &&
+                t1.pop_back();
         }
-        return leftIndex;   // leftIndex一定指向了最终数组末尾的下一个元素
+        return s1==t1;
     }
 int main()
 {
+    string s ="xywrrmp", t="xywrrmu#p";
+    backspaceCompare(s,t);
     vector<int>nums ={1}; int val=1;
-    removeElement(nums,val);
-    string s ="abcz"; int k=26;
+    // removeElement(nums,val);
+    // string s ="abcz"; int k=26;
     // cout<<smallestBeautifulString(s,k);
     int n = 2; vector<vector<int>> meetings = {{0,10},{1,2},{12,14},{13,15}};
     // int res=mostBooked(n, meetings); // {{0,10},{1,5},{2,7},{3,4}}  n=2
