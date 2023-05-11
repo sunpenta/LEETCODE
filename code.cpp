2a82cd691123d8918d82da4c3196137ad851595f
Auly <sunpentacle@icloud.com>
2023-05-11 12:59:41 +0000

    May 11, 2023, 8:59 PM

--- code.cpp
+++ code.cpp
@@ -78,6 +78,13 @@ using namespace std;
             if (i>0) 
                 dp[0][i]=dp[0][i-1];
         }
+         for (int i=0; i<m; ++i)
+        {
+            if (nums1[i]==nums2[0])
+                dp[i][0]=1;
+            if (i>0) 
+                dp[i][0]=dp[i-1][0];
+        }       
         for (int i=1; i<m; ++i)
         {
             for (int j=1; j<n; ++j)
