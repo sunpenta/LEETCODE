d7b10ab00cc044b5479965aaa818c8ea831f0fe6
Auly <sunpentacle@icloud.com>
2023-05-14 13:07:50 +0000

    May 14, 2023, 9:07 PM

--- array/removeElement(in place)/3moveZeros.cpp
+++ array/removeElement(in place)/3moveZeros.cpp
@@ -1,2 +1,15 @@
 // 移动零元素到数组尾端  l-283
 // 时间复杂度：O(n) 空间复杂度:O(1)
+#include <iostream>
+#include <vector>
+void moveZeros(vector<int>& nums);
+
+int main()
+{
+
+}
+
+void moveZeros(vector<int>& nums)
+{
+    
+}
