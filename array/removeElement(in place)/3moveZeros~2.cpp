97368bf136819d5b1cafe31ceadb2fb8b1627877
Auly <sunpentacle@icloud.com>
2023-05-14 13:41:29 +0000

    May 14, 2023, 9:41 PM

--- array/removeElement(in place)/3moveZeros~2.cpp
+++ array/removeElement(in place)/3moveZeros~2.cpp
@@ -1,1 +1,15 @@
-// 移动0元素
+// 移动0元素到数组尾端 - 冒泡  l-283
+# include<iostream>
+# include<vector>
+using namespace std;
+void moveZeros(vector<int>& nums);
+
+int main()
+{
+
+}
+
+void moveZeros(vector<int>& nums)
+{
+    
+}
