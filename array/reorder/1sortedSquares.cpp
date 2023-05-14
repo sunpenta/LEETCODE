979cd3baefd66c96452260cdd74301ffca7028e2
Auly <sunpentacle@icloud.com>
2023-05-14 14:03:17 +0000

    May 14, 2023, 10:03 PM

--- array/reorder/1sortedSquares.cpp
+++ array/reorder/1sortedSquares.cpp
@@ -3,4 +3,18 @@
 # include<iostream>
 # include<vector>
 using namespace std;
-vector<int> 
+vector<int> sortedSquares(vector<int>& nums);
+
+int main()
+{
+
+}
+
+vector<int> sortedSquares(vector<int>& nums)
+{
+    int i=0, j=nums.size()-1;
+    while (i<j)
+    {
+        
+    }
+}
