afde02d6ac8b8fa304328e39cd1bc7836bc0f9eb
Auly <sunpentacle@icloud.com>
2023-05-14 15:00:11 +0000

    May 14, 2023, 11:00 PM

--- BinarySearch/Foundation/search.cpp
+++ BinarySearch/Foundation/search.cpp
@@ -3,13 +3,26 @@
 # include<iostream>
 # include<vector>
 using namespace std;
+int search(vector<int>& nums, int target);
 
 int main()
 {
-
+    vector<int> nums={-1,0,3,5,9,12}; int target=9; // input
+    
 }
 
 int search(vector<int>& nums, int target)
 {
-    
+    int i=0, j=nums.size()-1;
+    while (i<=j)
+    {
+        int mid=(left+right)/2;
+        if (nums[mid]==target)
+            return mid;
+        else if (nums[mid]<target)
+            left=mid+1;
+        else
+            right=mid-1;
+    }
+    return -1;
 }
