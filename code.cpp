c862204d84502b036533f9a8e8dd84dac85beb8b
Auly <sunpentacle@icloud.com>
2023-05-14 08:17:16 +0000

    May 14, 2023, 4:17 PM

--- code.cpp
+++ code.cpp
@@ -149,7 +149,38 @@ using namespace std;
     }
 int main()
 {
-    
+        vector<int> nums={3,4,6,8};
+        int n=nums.size()/2;
+        sort(nums.begin(),nums.end());
+        map<int,int> mp;
+        for (int num:nums)
+            mp[num]++;
+        int score=0;
+        for (int i=1; i<=n && !nums.empty(); i++) // ith operation i<=n
+        {
+            if (!mp.empty())
+            {
+                for (auto it:mp)
+                {
+                    int num=it.first;
+                    if (num!=nums[i-1] && num%nums[i-1]==0) // nums[i-1]
+                    {
+                        cout<<i<<" " <<nums[i-1]<<" "<<num<<endl;
+                        if (--mp[num]==0)
+                            mp.erase(num);
+                        if (--mp[nums[i-1]]==0)
+                            mp.erase(nums[i-1]);
+                        
+                        nums.erase(find(nums.begin(),nums.end(),num));
+                        nums.erase(find(nums.begin(),nums.end(),nums[i-1]));
+                        score+=i*nums[i-1];
+                        break;
+                    }
+                }
+            }
+        } 
+        return 0;   
+    }
     // vector<vector<int>> questions={{21,2},{1,2},{12,5},{7,2},{35,3},{32,2},{80,2},{91,5},{92,3},{27,3},{19,1},{37,3},{85,2},{33,4},{25,1},{91,4},{44,3},{93,3},{65,4},{82,3},{85,5},{81,3},{29,2},{25,1},{74,2},{58,1}}; // {{3,2},{4,3},{4,4},{2,5}} {{21,2},{1,2},{12,5},{7,2},{35,3},{32,2},{80,2},{91,5},{92,3},{27,3},{19,1},{37,3},{85,2},{33,4},{25,1},{91,4},{44,3},{93,3},{65,4},{82,3},{85,5},{81,3},{29,2},{25,1},{74,2},{58,1},{85,1},{84,2},{27,2},{47,5},{48,4},{3,2},{44,3},{60,5},{19,2},{9,4},{29,5},{15,3},{1,3},{60,2},{63,3},{79,3},{19,1},{7,1},{35,1},{55,4},{1,4},{41,1},{58,5}}
     //cout<<mostPoints(questions)<<endl;
     //vector<int>nums={-6,40,-38,-36,-36,-4,-38,-43,8}; int k=2, x=2; // nums={1,-1,-3,-2,3}; int k=3, x=2  {5} 1 1 nums={-3,1,2,-3,0,-3}; int k=2, x=1 {-46,-34,-46}; int k=3, x=3
@@ -175,7 +206,6 @@ int main()
     // long long a = (static_cast<int64_t>(pow(2, 54)) - 1) % mod; // int64_t is long long
     // ***
     // -7,-8,7,5,7,1,6,0
-    return 0;
+    
     /*n =
 10*/
-}
