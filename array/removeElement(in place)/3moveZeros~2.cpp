c434567a2984c30b3759358aa886925a6d8d4b1a
Auly <sunpentacle@icloud.com>
2023-05-14 13:52:44 +0000

    May 14, 2023, 9:52 PM

--- array/removeElement(in place)/3moveZeros~2.cpp
+++ array/removeElement(in place)/3moveZeros~2.cpp
@@ -14,9 +14,9 @@ int main()
 void moveZeros(vector<int>& nums)
 {
     int i=0, j=0;
-    for (; i<nums.size()-1; i++)
+    for (; j<nums.size(); j++)
     {
-        if (nums[j]==0)
+        if (nums[j]!=0)
             swap(nums[i++],nums[j]);
     }
 }
