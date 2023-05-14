1b9eb2b392ab08a18089f6b776321cf7bac067a3
Auly <sunpentacle@icloud.com>
2023-05-14 12:42:56 +0000

    May 14, 2023, 8:42 PM

--- array/removeElement(in place)/1removeElement.cpp
+++ array/removeElement(in place)/1removeElement.cpp
@@ -1,10 +1,16 @@
 // 移除元素  l-27
-// 
+// ->移除val后的元素数
 # include<iostream>
 # include<vector>
 using namespace std;
 int removeElement(vector<int>& nums, int val);
+
 int main()
 {
 
+}
+
+int removeElement(vector<int>& nums, int val)
+{
+    
 }
