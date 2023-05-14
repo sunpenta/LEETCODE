42bf6a31084902520657af0f41b97b8a2aac96f3
Auly <sunpentacle@icloud.com>
2023-05-14 12:59:53 +0000

    May 14, 2023, 8:59 PM

--- array/removeElement(in place)/2removeDuplicates.cpp
+++ array/removeElement(in place)/2removeDuplicates.cpp
@@ -1,5 +1,5 @@
 // 删除有序数组的重复元素 - 快慢指针  l-26
-// -> 无重复元素个数k(保留1个)；原数组改变，前k个是无重复元素(原顺序不变)
+// -> 无重复元素个数k(保留1个)；原数组前k个是无重复元素(原顺序不变)
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,7 +8,8 @@ int deleteDuplicates(vector<int>& nums);
 int main()
 {
     vector<int> nums={0,0,1,1,1,2,2,3,3,4}; // input
-    cout<<deleteDuplicates(nums)<<endl; // output: 5 {0,1,2,3,4,2,2,3,3,4}
+    int res=deleteDuplicates(nums); // output: 5 {0,1,2,3,4,2,2,3,3,4}
+    cout<<res<<endl; 
     for (int num:nums)
         cout<< num << " ";
     return 0;  
