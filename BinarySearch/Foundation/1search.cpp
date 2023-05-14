ab4259e065833a700d2c0fdd7d5600d92ca1b71a
Auly <sunpentacle@icloud.com>
2023-05-14 15:09:33 +0000

    May 14, 2023, 11:09 PM

--- BinarySearch/Foundation/1search.cpp
+++ BinarySearch/Foundation/1search.cpp
@@ -16,8 +16,8 @@ int main()
 
 int search(vector<int>& nums, int target)
 {
-    int i=0, j=nums.size()-1;
-    while (i<=j)
+    int left=0, right=nums.size()-1;
+    while (left<=right)
     {
         int mid=(left+right)/2;
         if (nums[mid]==target)
