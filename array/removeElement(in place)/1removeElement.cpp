62024c33bb471912390ae82af03a25fe19a5b178
Auly <sunpentacle@icloud.com>
2023-05-14 12:47:55 +0000

    May 14, 2023, 8:47 PM

--- array/removeElement(in place)/1removeElement.cpp
+++ array/removeElement(in place)/1removeElement.cpp
@@ -1,5 +1,5 @@
 // 移除元素  l-27
-// ->移除val后的元素数
+// ->移除val后的元素数k, nums原数组前k位是移除后元素
 # include<iostream>
 # include<vector>
 using namespace std;
@@ -7,10 +7,20 @@ int removeElement(vector<int>& nums, int val);
 
 int main()
 {
-
+    vector<int> nums={0,1,2,3,3,0,4,2}; int val=2; // input;
+    int res=removeElement(nums,val);
+    cout<<res<<endl;
+    return 0; 
 }
 
 int removeElement(vector<int>& nums, int val)
 {
-    
+    int i=0, j=0;
+    for (; j<nums.size(); j++)
+    {
+        if (nums[j]!=val)
+            nums[i++]=nums[j];
+        
+    }
+    return i;
 }
