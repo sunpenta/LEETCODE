963a376e308a741743039f6c8075ad98646553e5
Auly <sunpentacle@icloud.com>
2023-06-07 16:23:00 +0000

    Jun 8, 2023, 12:23 AM

---
+++ BinarySearch/Application/isPerfectSquare.cpp
@@ -0,0 +1,28 @@
+// 完全平方根  l-367
+#include <iostream>
+using namespace std;
+int mySqrt(int x);
+
+int main()
+{
+    int x=7; // intput
+    int res=mySqrt(x); // output:2
+    cout<<res<<endl;
+    return 0;
+}
+
+int mySqrt(int x)
+{
+    int left=0, right=x/2;
+    while (left<=right)
+    {
+        int mid=(left+right)/2;
+        if ((long)mid*mid==x) 
+            return mid;
+        else if ((long)mid * mid > x)
+            right=mid-1;
+        else
+            left=mid+1;
+    }
+    return left-1;
+}
