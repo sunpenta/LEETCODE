2156f12accb22f244748ad906d7a3bac9d20b737
Auly <sunpentacle@icloud.com>
2023-06-07 16:28:37 +0000

    Jun 8, 2023, 12:28 AM

--- BinarySearch/Application/findClosestElements.cpp
+++ BinarySearch/Application/findClosestElements.cpp
@@ -13,13 +13,14 @@ int main()
     return 0;
 }
 
+// // [mid, x, mid+k)
 vector<int> findClosestElements(vector<int>& nums, int k, int x)
 {
     int left=0, right=nums.size()-k;
     while (left<right)
     {
-        int mid=left+(right-left)/2;         //       x
-        if (x-nums[mid]>nums[mid+k]-x) // [mid,mid+k)
+        int mid=left+(right-left)/2;     
+        if (x-nums[mid]>nums[mid+k]-x) 
             left=mid+1;
         else
             right=mid;
