3e3a6295a0f687d24ebe013c5845f0c0bca33330
Auly <sunpentacle@icloud.com>
2023-06-07 15:38:34 +0000

    Jun 7, 2023, 11:38 PM

--- BinarySearch/Application/minCapability.cpp
+++ BinarySearch/Application/minCapability.cpp
@@ -20,7 +20,7 @@ int minCapability(vector<int>& nums, int k)
         int count=0;
         for (int i=0; i<nums.size(); i++)
         {
-            if (nums[i]<=k)
+            if (nums[i]<=mid)
             {
                 count++;
                 i++;
