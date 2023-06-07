39608ed90d3a0d970d3f73c0c388a75447548ebf
Auly <sunpentacle@icloud.com>
2023-06-07 15:21:21 +0000

    Jun 7, 2023, 11:21 PM

--- Bitwise/2.1singleNumber.cpp
+++ Bitwise/2.1singleNumber.cpp
@@ -1,12 +1,14 @@
 // 只出现1次的数字 - 异或  l-136
-// 只有1个元素出现1次，其余元素均出现两次; nums[i]∈[INT_MIN,INT_MAX]; 时间复杂度:O(n),空间复杂度:O(1)
-#include <bits/stdc++.h>
+// 只有1个元素出现1次，其余元素均出现两次; nums[i]∈[INT_MIN,INT_MAX]; 
+// 时间复杂度:O(n),空间复杂度:O(1)
+#include <iostream>
+#include <vector>
 using namespace std;
 int singleNumber(vector<int>& nums);
 
 int main()
 {
-    vector<int>nums={2,2,3,3,1,4,4}; // 输入
+    vector<int> nums={2,2,3,3,1,4,4}; // 输入
     cout<<singleNumber(nums)<<endl; // 输出
     return 0;
 }
@@ -15,7 +17,7 @@ int main()
 int singleNumber(vector<int>& nums)
 {
     int ans=0;
-    for (auto &num:nums)
+    for (int num:nums)
     {
         ans^=num; 
     }
