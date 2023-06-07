d2d137e24c43ff9a3fa7760a28a722c38e5a0b53
Auly <sunpentacle@icloud.com>
2023-06-07 16:10:29 +0000

    Jun 8, 2023, 12:10 AM

--- BinarySearch/Application/sqrt~1.cpp
+++ BinarySearch/Application/sqrt~1.cpp
@@ -2,19 +2,19 @@
 // -> √x 向下取整 
 #include <iostream>
 using namespace std;
-int sqrt(int x);
+int mySqrt(int x);
 
 int main()
 {
     int x=7; // intput
-    int res=sqrt(x); // output:2
+    int res=mySqrt(x); // output:2
     cout<<res<<endl;
     return 0;
 }
 
-int sqrt(int x)
+int mySqrt(int x)
 {
     int i=0;
-    for (; i*i<=x; i++) {}
+    for (; (long)i*i<=x; i++) {}
     return i-1;
 }
