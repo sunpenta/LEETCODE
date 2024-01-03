a1a600f9be03f0facb1df05e0e99b5e50a873ca1
sun <sunpentacle@icloud.com>
2024-01-03 22:24:32 +0800

    1248:滑动窗口 k个奇数，整理滑动窗口目录

---
+++ slidingWindow/others/1numberOfSubarrays.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int numberOfSubarrays(vector<int> &nums, int k);
+
+int main()
+{
+    vector<int> nums{2,2,2,1,2,2,1,2,2,2}; int k=2;
+    int res = numberOfSubarrays(nums,k);
+    cout << res << endl;
+    return 0;
+}
+
+int numberOfSubarrays(vector<int> &nums, int k)
+{
+    int n = nums.size(), res = 0, count = 0, i = 0, j = 0;
+    for (; j < n; j++)
+    {
+        if (nums[j] % 2)
+        {
+            --k;
+            count =0;
+        }
+
+        while (k == 0)
+        {
+            k += nums[i++] & 1;
+            count++;
+        }
+        res+=count;
+    }
+    return res;
+}
